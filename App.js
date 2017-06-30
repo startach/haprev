1c3507f1c897e5e3d552887638d2a4eede9df44d
Elad Cohen <eladc.android@gmail.com>
2017-06-30 17:48:44 +0300

    Added eslint & nativeBase support

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
-import App from './src/app/App'
+import App from './src/app/App';
 
-export default App
+export default App;
--- src/views/App.js
+++ src/views/App.js
@@ -1,28 +1,82 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import { Container, Content, Button, Text, connectStyle } from "native-base";
+import Expo from "expo";
+import PropTypes from 'prop-types';
 
-export default class App extends React.Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+  }
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+
+    this.setState({ isReady: true });
+  }
   render() {
-    const { navigate } = this.props.navigation;
+    const styles = this.props.style;
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Button title = "Dashboard" onPress = { () => navigate('Dashbaord')} />
-        <Button title = "Search" onPress = { () => navigate('Search')} />
-
-
-      </View>
+      <Container style={styles.container}>
+        <Content style={styles.content}>
+          <View style={styles.contentView}>
+            <Text style={styles.title}>הרשמו והפכו לחלק ממהפכה של שמחה!</Text>
+            <View style={styles.buttonsContainer}>
+              <Button block rounded style={styles.button}>
+                <Text>התחברו באמצעות F</Text>
+              </Button>
+            </View>
+            <View style={styles.buttonsContainer}>
+              <Button block rounded style={styles.button}>
+                <Text>צרו חשבון חדש</Text>
+              </Button>
+            </View>
+          </View>
+        </Content>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
-});
+  content: {
+    flex: 1
+  },
+  contentView: {
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 18,
+    fontFamily: "Roboto_medium"
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20
+  },
+  buttonsContainer: {
+    justifyContent: "center",
+    alignItems: "center"
+  }
+};
+
+App.propTypes = {
+  style: PropTypes.object.isRequired,
+}
+// connect the component to the theme
+export default connectStyle("yourTheme.CustomComponent", styles)(App);
