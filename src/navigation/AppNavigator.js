a8b7e5a5ab8cb1876396d9cd74f33d164426053f
Elad Cohen <eladc.android@gmail.com>
2017-07-14 04:23:15 +0300

    Finished Home screen layout design

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,12 +4,21 @@ import DashboardScreen from '../views/Dashboard';
 import SearchScreen from '../views/search';
 import HospitalScreen from '../views/hospitalList/HospitalList';
 
-
-export default StackNavigator({
-  Home: { screen: HomeScreen },
-  Dashbaord: { screen: DashboardScreen },
-  Search: { screen: SearchScreen },
-  Hospital: { screen: HospitalScreen },
-}, {
-  initialRouteName: 'Hospital',
-});
+export default StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      headerMode: 'none',
+      header: null,
+      navigationOptions: {
+        header: null
+      }
+    },
+    Dashbaord: { screen: DashboardScreen },
+    Search: { screen: SearchScreen },
+    Hospital: { screen: HospitalScreen }
+  },
+  {
+    initialRouteName: 'Hospital'
+  }
+);
--- src/views/App.js
+++ src/views/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, TouchableHighlight } from 'react-native';
 import {
   Container,
   Content,
   Button,
   Text,
   connectStyle,
-  Icon,
+  Icon
 } from 'native-base';
 import Expo from 'expo';
 import PropTypes from 'prop-types';
+
+const bg = require('../images/bg.jpg');
+const logo = require('../images/logo.png');
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      isReady: false,
+      isReady: false
     };
   }
   async componentWillMount() {
     await Expo.Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'),
+      Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf')
     });
 
     this.setState({ isReady: true });
@@ -33,48 +36,89 @@ class App extends React.Component {
       return <Expo.AppLoading />;
     }
     return (
-      <Container style={styles.container}>
-        <Text style={styles.loginExist}>חשבון קיים</Text>
-        <Content contentContainerStyle={styles.contentContainer}>
-          <View style={styles.contentView}>
-            <Text style={styles.title}>הרשמו והפכו לחלק ממהפכה של שמחה!</Text>
-            <View style={styles.buttonsContainer}>
-              <Button rounded style={styles.button}>
-                <Icon name="logo-facebook" />
-                <Text>התחברו באמצעות </Text>
-              </Button>
+      <Image source={bg} style={styles.backgroundImage}>
+        <TouchableHighlight
+          style={styles.loginExistView}
+          onPress={() => this.setState({ toggled: !this.state.toggled })}
+        >
+          <Text style={styles.loginExist}>חשבון קיים</Text>
+        </TouchableHighlight>
+        <Container style={styles.container}>
+          <Content contentContainerStyle={styles.contentContainer}>
+            <View style={styles.contentView}>
+              <Image source={logo} style={styles.logoImage} />
+              <View style={styles.titlesContainer}>
+                <Text style={styles.title}>הרשמו והפכו לחלק</Text>
+                <Text style={styles.subtitle}>ממהפכה של שמחה!</Text>
+              </View>
+              <View style={styles.buttonsContainer}>
+                <Button rounded style={styles.button}>
+                  <Icon name="logo-facebook" />
+                  <Text>התחברו באמצעות </Text>
+                </Button>
+              </View>
+              <View style={styles.buttonsContainer}>
+                <Button rounded style={styles.registerButton}>
+                  <Text>צרו חשבון חדש</Text>
+                </Button>
+              </View>
+              <View style={styles.orTextContainer}>
+                <Text style={styles.orText}>או</Text>
+              </View>
+              <View style={styles.buttonsContainer}>
+                <Button rounded bordered style={styles.guestButton}>
+                  <Text style={styles.guestButtonText}>התחברו כאורחים</Text>
+                </Button>
+              </View>
             </View>
-            <View style={styles.buttonsContainer}>
-              <Button rounded style={styles.registerButton}>
-                <Text>צרו חשבון חדש</Text>
-              </Button>
-            </View>
-          </View>
-        </Content>
-      </Container>
+          </Content>
+        </Container>
+      </Image>
     );
   }
 }
 
 const styles = {
-  container: {
+  backgroundImage: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-  contentContainer: {
-    flex: 1,
+    width: undefined,
+    height: undefined,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 10
+  },
+  logoImage: {
+    alignSelf: 'center',
+    width: 150,
+    resizeMode: 'contain',
     padding: 20,
+    margin: 0
+  },
+  contentView: {
+    flex: 0,
+    padding: 0,
+    margin: 0
+  },
+  container: {
+    flex: 1
+  },
+  contentContainer: {
+    flex: 0
+  },
+  loginExistView: {
+    flex: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    alignSelf: 'flex-end'
   },
   loginExist: {
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
     fontSize: 15,
     textAlign: 'right',
-    padding: 20,
+    paddingTop: 20,
+    paddingRight: 20,
+    paddingBottom: 0,
     textDecorationLine: 'underline',
+    color: '#FFF'
   },
   title: {
     justifyContent: 'center',
@@ -82,25 +126,56 @@ const styles = {
     fontSize: 28,
     fontFamily: 'Roboto_medium',
     textAlign: 'center',
+    color: '#FFF'
+  },
+  titlesContainer: {
+    padding: 20
+  },
+  subtitle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 28,
+    fontFamily: 'Roboto_medium',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#FFF'
   },
   button: {
     justifyContent: 'center',
     marginTop: 20,
-    width: '100%',
+    width: '100%'
   },
   registerButton: {
     backgroundColor: 'rgb(211,75,53)',
     justifyContent: 'center',
     marginTop: 20,
+    width: '100%'
+  },
+  guestButton: {
+    justifyContent: 'center',
+    marginTop: 20,
     width: '100%',
+    borderColor: '#FFF'
+  },
+  guestButtonText: {
+    color: '#FFF'
   },
   buttonsContainer: {
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  orTextContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 10
+  },
+  orText: {
+    textAlign: 'center',
+    color: '#FFF'
+  }
 };
 
 App.propTypes = {
-  style: PropTypes.object.isRequired,
+  style: PropTypes.object.isRequired
 };
 // connect the component to the theme
 export default connectStyle('yourTheme.CustomComponent', styles)(App);
