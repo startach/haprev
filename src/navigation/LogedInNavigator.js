6738d91de9be95ea9b6f3d6db353c521bc223241
dansha <shamir.dan@gmail.com>
2017-07-30 09:42:34 +0300

    removed naviagtion logging

---
+++ src/navigation/LogedInNavigator.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { TabNavigator } from 'react-navigation';
+import { Entypo } from '@expo/vector-icons';
+import DashboardScreen from '../views/Dashboard';
+import SearchScreen from '../views/search';
+import ContactScreen from '../views/Contacts/ContactsView';
+import ChatScreen from '../views/chat/ChatView';
+import ProfileScreen from '../views/profile/ProfileView';
+
+import HospitalScreen from '../views/hospitalList/HospitalList';
+
+const LoggedInNavigator = TabNavigator(
+  {
+    Contacts: { screen: ContactScreen,
+      navigationOptions: {
+        tabBarLabel: '',
+        tabBarIcon: ({ tintColor }) => (
+          <Entypo
+            name="500px"
+            size={32}
+            style={{ color: tintColor }}
+          />
+        ),
+      },
+    },
+    Chat: { screen: ChatScreen },
+    Dashbaord: { screen: DashboardScreen },
+    Profile: { screen: ProfileScreen },
+    Search: { screen: SearchScreen },
+    Hospital: { screen: HospitalScreen },
+  },
+  {
+    initialRouteName: 'Contacts',
+    tabBarPosition: 'bottom',
+    swipeEnabled: false,
+    showLabel: false,
+
+  },
+);
+
+export default () => (
+  <LoggedInNavigator onNavigationStateChange={null} />
+);
+
