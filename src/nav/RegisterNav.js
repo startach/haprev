6f34cd1d2789feecf2199a6b966cc6e54cb68d64
Yishai Brown <ybrown.jm@gmail.com>
2018-02-26 22:09:14 +0200

    versions change

--- src/nav/RegisterNav.js
+++ src/nav/RegisterNav.js
@@ -23,5 +23,5 @@ export default StackNavigator({
             navigationOptions: { header: null, },
         },
     },
-        { initialRouteName: 'Splash',}
+        { initialRouteName: 'Register',}
 )
