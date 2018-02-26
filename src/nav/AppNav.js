9fae2f21e0a451a32d15d693c1e60e6852e1001d
chenezra <chen.h.ezra@gmail.com>
2018-02-26 19:34:20 +0200

    navigators + admin activities

--- src/nav/AppNav.js
+++ src/nav/AppNav.js
@@ -1,46 +1,45 @@
-import { DrawerNavigator } from 'react-navigation';
-import HomeNav from './HomeNav'
-import InstitutesNav from './InstitutesNav'
-import ProfileNav from './ProfileNav'
-import ActivitiesNav from './ActivitiesNav'
-import ActivitiesAdminNav from './ActivitiesAdminNav'
-import HelpNav from './HelpNav'
+import React from 'react'
+import { FontAwesome } from '@expo/vector-icons';
+import { DrawerNavigator, StackNavigator, NavigationActions } from 'react-navigation';
+import Home from "../views/home/Home";
+import Institutes from "../views/institutes/Institute";
+import Profile from "../views/profile/Profile";
+import Activities from "../views/activities/Activities";
+import AdminActivities from "../views/adminActivities/AdminActivities";
+import AdminActivity from "../views/adminActivities/AdminActivity";
+import Help from "../views/help/Help";
+
+appNavigationOptions = ({ navigation }) => ({
+    headerRight: <FontAwesome style={{paddingHorizontal:20}} name="chevron-left" size={24}
+                              onPress={ () => {navigation.goBack() }} />,
+    headerLeft: <FontAwesome style={{paddingHorizontal:20}} name="navicon" size={24}
+                             onPress={ () => { navigation.navigate('DrawerOpen')}} />,
+});
+
+homeNavigationOptions = ({ navigation }) => ({
+    headerLeft: <FontAwesome style={{paddingHorizontal:20}} name="navicon" size={24}
+                             onPress={ () => { navigation.navigate('DrawerOpen')}} />,
+});
+
+function RenderStackNavigator(screens, isHome) {
+    const Nav = {};
+    for (let screen of screens){
+        Nav[screen.screenName.name] = {
+            screen: screen.screenName,
+            headerMode: 'float',
+            headerTransitionPreset: 'fade-in-place',
+            navigationOptions: {title: screen.title}
+        }
+    }
+    return StackNavigator(Nav, {navigationOptions: isHome ? homeNavigationOptions: appNavigationOptions});
+}
 
 export default DrawerNavigator({
-    Home: {
-        screen: HomeNav,
-        navigationOptions: {
-            drawerLabel: 'מסך הבית',
-        }
-    },
-    Institutes: {
-        screen: InstitutesNav,
-        navigationOptions: {
-            drawerLabel: 'בתי חולים',
-        }
-    },
-    Profile: {
-        screen: ProfileNav,
-        navigationOptions: {
-            drawerLabel: 'הפרופיל שלי',
-        }
-    },
-    Activities: {
-        screen: ActivitiesNav,
-        navigationOptions: {
-            drawerLabel: 'ההתנדבויות שלי',
-        }
-    },
-    ActivitiesAdmin: {
-        screen: ActivitiesAdminNav,
-        navigationOptions: {
-            drawerLabel: 'ניהול התנדבויות',
-        }
-    },
-    Help: {
-        screen: HelpNav,
-        navigationOptions: {
-            drawerLabel: 'עזרה',
-        }
-    },
+    Home: RenderStackNavigator([{screenName: Home, title: 'מסך הבית'}], true),
+    Institutes: RenderStackNavigator([{screenName: Institutes, title: 'בתי חולים'}]),
+    Profile: RenderStackNavigator([{screenName: Profile, title: 'הפרופיל שלי'}]),
+    Activities: RenderStackNavigator([{screenName: Activities, title: 'ההתנדבויות שלי'}]),
+    ActivitiesAdmin: RenderStackNavigator([{screenName: AdminActivities, title: 'ניהול התנדבויות'},
+        {screenName: AdminActivity, title: 'התנדבות'}]),
+    Help: RenderStackNavigator([{screenName: Help, title: 'עזרה'}])
 })
---
+++ src/views/adminActivities/AdminActivity.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react'
+import {View, Text, Button, StyleSheet, FlatList} from 'react-native'
+import { MaterialDialog } from 'react-native-material-dialog';
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'space-around',
+    },
+    h1:{
+        textAlign:'center',
+        fontSize:18,
+        padding:10,
+    },
+    h2:{
+        textAlign:'center',
+        fontSize:16,
+        paddingTop:30,
+    },
+    participantItem: {
+        marginHorizontal: '10%',
+        width: '80%',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        borderTopColor: '#333',
+        borderTopWidth: StyleSheet.hairlineWidth,
+    },
+    img:{
+        height: 40,
+        width: 40,
+        backgroundColor:'#080',
+        margin: 5,
+    },
+    bottomButtons:{
+        bottom:0,
+    },
+    button:{
+        margin: 20
+    }
+})
+
+const ParticipantItem = ({participant}) => {
+    return <View style={styles.participantItem}>
+        <View style={styles.img}></View>
+        <Text>{participant.name}</Text>
+    </View>
+}
+
+class AdminActivity extends Component
+{
+    state = {cancelDialogVisible: false};
+    cancelActivity() {
+        this.setState({cancelDialogVisible: false});
+        console.log('cancel activity');
+    };
+
+    render() {
+        const {params} = this.props.navigation.state;
+        const activity = params ? params.activity : null;
+
+
+        const toggleCancelDialog = () => this.setState({cancelDialogVisible: true})
+
+        return (
+            <View style={styles.container}>
+                <Text style={styles.h2}> התנדבות {activity.date} </Text>
+                <Text style={styles.h1}> לפעילות זו
+                    רשומים {activity.participants ? activity.participants.length : 0} מתנדבים </Text>
+                <FlatList data={activity.participants}
+                          renderItem={({item}) => <ParticipantItem participant={item}/>}
+                          keyExtractor={(item) => item.id}/>
+                <View style={styles.bottomButtons}>
+                    <Button style={styles.button} title="בטל התנדבות" onPress={toggleCancelDialog}/>
+                </View>
+                <MaterialDialog
+                    title="האם לבטל את ההתנדבות?"
+                    visible={this.state.cancelDialogVisible}
+                    onOk={() => this.cancelActivity()}
+                    onCancel={() => this.setState({cancelDialogVisible: false})}
+                    okLabel='בטל התנדבות'
+                    cancelLabel='השאר התנדבות'
+                    colorAccent='#00c'>
+                    <View></View>
+                </MaterialDialog>
+            </View>
+        )
+    }
+}
+
+export default AdminActivity
