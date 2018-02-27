2f37219c36550756da21b6b5eb7334db5f8200c6
idanlevi <idanlevi2@gmail.com>
2018-02-27 17:52:20 +0200

    help

--- src/views/help/Help.js
+++ src/views/help/Help.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import HelpView from './HelpView'
 import { connect } from 'react-redux'
@@ -29,24 +28,4 @@ return ({
 }
 
 export default connect(mapStateToProps)(Help)
-=======
-import React from 'react'
-import {View,Text,StyleSheet} from 'react-native'
-
-const styles = StyleSheet.create({
-    container:{
-        marginTop:50
-    }
-})
-
-const Help = (props) =>
-{
-    return(
-        <View style={styles.container}>
-            <Text> this will be Help screen </Text>
-        </View>
-    )
-}
-
-export default Help
->>>>>>> 6f83c1c3d0a5c3f9c80ee9fde336779a17c9172c
+//
