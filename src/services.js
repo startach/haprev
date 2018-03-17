e74be5c42d43e65e686ac51496bbe1ea0d389048
Andrey Markovytch <andrey.markovytch@argus-sec.com>
2018-03-18 01:03:54 +0200

    Home Screen 2

--- src/services.js
+++ src/services.js
@@ -3,6 +3,22 @@ const DELAY = 200;
 
 const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
 
+export const Authorize = async appId => {
+      await timeout(DELAY);
+      return ({
+        userId:'234',
+        appId:appId,
+        first: 'dan',
+        last: 'shamir',
+        phone: '054-5283004',
+        email:'shamir.dan@gmail.com',
+        avatar: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAy-AAAAJDQ2NDI3ZTFlLWE1YzAtNDBjOC1iMzJhLTVkYTQxN2MzNjdmMQ.jpg',
+        coordinator:['zzz'],
+        messages:[],  
+      });
+};
+  
+
 export const HelpRequest = async (first,last,email,content) => {
   await timeout(DELAY);
   return ({request:'ok'});
