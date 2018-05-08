52a611da286fe2fb8782a3da88534ea7365bfe7d
dansha <shamir.dan@gmail.com>
2018-05-08 10:06:52 +0300

    add smart container to createActivity

---
+++ src/views/adminActivities/CreateActivity.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import {
+    View,
+    Text,
+    TextInput,
+    FlatList,
+    TouchableHighlight,
+    KeyboardAvoidingView,
+    TouchableOpacity,
+    StyleSheet,
+    Modal} from "react-native";
+import CreateActivityView from './CreateActivityView';
+
+    class CreteActivity extends Component{
+        render(){
+            return (
+                <CreateActivityView>
+                </CreateActivityView>
+            )}
+    }   
+    
+    export default CreteActivity;
+
+
