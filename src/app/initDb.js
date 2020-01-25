07890b048416e17d4c51acfb6aad2ece82fd0359
idanlevi1 <idanlevi2@gmail.com>
2020-01-25 23:11:18 +0200

    env

--- src/app/initDb.js
+++ src/app/initDb.js
@@ -1,16 +1,24 @@
 import * as firebase from 'firebase'
-// import { keys } from './keys'
-import { ENV, API_KEY, AUTH_DOMAIN, DB_URL, PROJECT_ID, STORAGE_BUCKET, MSG_SENDER_ID } from 'react-native-dotenv'
+import { keys } from './keys'
+// import { ENV, API_KEY, AUTH_DOMAIN, DB_URL, PROJECT_ID, STORAGE_BUCKET, MSG_SENDER_ID } from 'react-native-dotenv'
 
-console.log("TCL: ENV", ENV)
+// console.log("TCL: ENV", ENV)
 
+// const firebaseConfig = {
+//   apiKey: API_KEY,
+//   authDomain: AUTH_DOMAIN,
+//   databaseURL: DB_URL,
+//   projectId: PROJECT_ID,
+//   storageBucket: STORAGE_BUCKET,
+//   messagingSenderId: MSG_SENDER_ID
+// }
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  databaseURL: DB_URL,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MSG_SENDER_ID
+  apiKey: keys.apiKey,
+  authDomain: keys.authDomain,
+  databaseURL: keys.databaseURL,
+  projectId: keys.projectId,
+  storageBucket: keys.storageBucket,
+  messagingSenderId: keys.messagingSenderId
 }
 
 export default () => {
